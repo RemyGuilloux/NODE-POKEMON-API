7e2edede1935c72be12a7a2826364af2f721233d
Remy <38971092+RemyGuilloux@users.noreply.github.com>
2022-05-18 20:00:32 +0200

    initialisation projet node-pokemon-api

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+const port = 3000 
+
+app.get('/', (req, res) => res.send('Hello, Express 3 !'))
+
+app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
