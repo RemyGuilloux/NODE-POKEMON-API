903d0979e9d73a70c44ff0ee8a6a8d985af3533d
Remy <38971092+RemyGuilloux@users.noreply.github.com>
2022-05-18 23:45:14 +0200

    affichage Json + methode message personalisé

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const { success } = require('./helper.js')
+let pokemons = require('./mock-pokemon');
 
 const app = express()
-const port = 3000 
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express 3 !'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'Un pokemon a bien été trouvé.'
+    res.json(success(message, pokemon))
+
+})
+
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokémons a bien été récupérée'
+    res.json(success(message,pokemons))
+})
 
 app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
