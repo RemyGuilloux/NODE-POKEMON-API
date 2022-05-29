e2ded36abb9285db278d32053ac0eb358f0aa663
Remy <38971092+RemyGuilloux@users.noreply.github.com>
2022-05-29 14:40:59 +0200

    ajout de validateur supplementaire sur les propriété du pokémon

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -7,7 +7,11 @@ module.exports = (sequelize, DataTypes) => {
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          notEmpty: {msg: `Le nom du pokemon ne peut pas être un espace vide`},
+          notNull: { msg: `le nom est une propriété requise`}
+        }
       },
       hp: {
         type: DataTypes.INTEGER,
@@ -19,11 +23,19 @@ module.exports = (sequelize, DataTypes) => {
       },
       cp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isInt: { msg: `Utilisez uniquement des nombres entiers pour les dégats`},
+          notNull: { msg: `Les dégats ne peuvent pas être null.`}
+        }
       },
       picture: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isUrl: { msg: `La photo du pokémon doit être une Url valide`},
+          notNull: { msg: `La photo du pokémon ne peut pas être null`}
+        }
       },
       types: {
         type: DataTypes.STRING,
