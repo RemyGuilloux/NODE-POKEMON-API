c6ec00f793956f36767b5fb8ad9fdf9c020a5e62
Remy <38971092+RemyGuilloux@users.noreply.github.com>
2022-05-30 21:33:27 +0200

    ajout d'une contrainte d'unicité pour le nom unique

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -10,6 +10,9 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: {
+          msg: 'Le nom est déjà pris.'
+        },
         validate: {
           notEmpty: {msg: `Le nom du pokemon ne peut pas être un espace vide`},
           notNull: { msg: `le nom est une propriété requise`}
