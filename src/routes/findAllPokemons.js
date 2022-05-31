4f5bb3d9862f5ea66bf167280598d3abccbd855e
Remy <38971092+RemyGuilloux@users.noreply.github.com>
2022-05-31 10:12:23 +0200

    ajout de query param, recherche d'un pokemon par son nom

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,17 @@
+const pokemons = require('../db/mock-pokemon')
 const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name){
+      const name = req.query.name
+      return Pokemon.findAll({where: {name: name}})
+      .then( pokemons => {
+        const message = `Il y a ${pokemons.length} pokémons qui correspondent au terme de la recherche ${name}.`
+        res.json({message, data: pokemons })
+      })
+    } else {
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +20,6 @@ module.exports = (app) => {
         const message = `La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`
         res.status(500).json({message, data: error})
       })
+    }
   })
 }
